d2126cdb3a2b3a7905384fb18922135a642bf591
LucasGuerraS <lucasgasardo@gmail.com>
2022-11-23 15:52:08 -0300

    🐛 fix imports

--- test/Spacebear.test.sol
+++ test/Spacebear.test.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "forge-std/Test.sol";
+import "../lib/forge-std/src/Test.sol";
 import "../src/Spacebear.sol";
 
 contract SpacebearTest is Test {
